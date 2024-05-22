2c2d1e6c144ea621d1968c5b32ea6e22055f6c1c
kareem7199 <kareemtameregy@gmail.com>
2024-05-22 21:25:29 +0300

    fix : change cart validator

--- validators/cartValidator.js
+++ validators/cartValidator.js
@@ -4,13 +4,27 @@ const schema = Joi.object({
     id: Joi.string().required(),
     items: Joi.array().items(
         Joi.object({
-            id: Joi.string().required(),
+            _id: Joi.string().required(),
             name: Joi.string().required(),
             images: Joi.array().items(Joi.string()).min(1).max(6).required(),
             price : Joi.number().min(1).required() ,
             quantity : Joi.number().integer().min(1).required()
         })
-    ).required()
+    ).required() ,
+    deliveryMethod : Joi.object({
+        _id : Joi.string().required() ,
+        name : Joi.string().required() ,
+        cost : Joi.number().min(0).required() ,
+        deliveryTime : Joi.string().required()
+    }).optional() ,
+    address : Joi.object({
+        firstName : Joi.string().required() ,
+        lastName : Joi.string().required() ,
+        phone : Joi.string().required() ,
+        street : Joi.string().required() ,
+        city : Joi.string().required() ,
+        state : Joi.string().required()
+    }).optional()
 }).options({ stripUnknown: true });
 
 const validateCart = (cart) => {
